3fbf2742830c959ba28abaccb04769f50b576779
David <143638937+Cheessse@users.noreply.github.com>
2024-04-11 21:45:23 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
 import './App.css';
 import SearchBar from './components/SearchBar/SearchBar';
@@ -7,6 +7,8 @@ import ImageGallery from './components/ImageGallery/ImageGallery.jsx';
 import ErrorMessage from './components/ErrorMessage/ErrorMessage.jsx';
 import Loader from './components/Loader/Loader.jsx'
 import LoadMoreBtn from './components/LoadMoreBtn/LoadMoreBtn.jsx'
+import ImageModal from './components/ImageModal/ImageModal.jsx';
+
 
 function App() {
   const [query, setQuery] = useState('')
@@ -15,12 +17,11 @@ function App() {
   const [totalResults, setTotalResults] = useState(0)
   const [isError, setError] = useState(null)
   const [isLoading, setLoading] = useState(false)
-
-
-
-
+  const [isOpen, setIsOpen] = useState(false)
+  const [selectedImage, setSelectedImage] = useState(null);
 
   useEffect(() => {
+    if (!query) return;
     const load = async () => {
       try {
         setLoading(true)
@@ -50,7 +51,22 @@ function App() {
   }
 
   const handleLoadMore = () => {
-    setPage(page+1)
+    setPage(page + 1)
+  }
+
+  const modalOpen = (image) => {
+    setIsOpen(true)
+    setSelectedImage(image)
+  }
+
+  console.log(selectedImage)
+  console.log(isOpen)
+
+
+
+    const modalClose = () => {
+      setIsOpen(false)
+      setSelectedImage(null)
   }
 
   return (
@@ -58,8 +74,13 @@ function App() {
       <SearchBar onSubmit={handleSubmit} />
       {isLoading && <Loader />}
       {isError && <ErrorMessage ErrorMessage={isError} />}
-      <ImageGallery images={images} />
+      <ImageGallery images={images} onImageClick={modalOpen} />
       {(totalResults > 0 && images.length < totalResults) && <LoadMoreBtn onClick={handleLoadMore} />}
+      <ImageModal
+        images={selectedImage}
+        isOpen={isOpen}
+        onRequestClose={modalClose}
+      />
             <Toaster
             position="top-right"
             reverseOrder={false}
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,13 +1,24 @@
+import { useEffect } from "react";
 import ImageCard from "../ImageCard/ImageCard"
 import css from './ImageGallery.module.css'
 
-const ImageGallery = ({ images }) => {
+const ImageGallery = ({ images, onImageClick }) => {
+
+    useEffect(() => {
+        window.scrollBy({
+          top: 800,
+          left: 0,
+          behavior: "smooth"
+        });
+    }, [images]); 
   
     return (
       <ul className={css.imageGallery}>
         { images.map((image) => (
             <li key={image.id}>
-              <ImageCard image={image} />
+            <ImageCard
+              image={image}
+              onClick={() => onImageClick(image)} />
             </li>
           ))}
       </ul>
