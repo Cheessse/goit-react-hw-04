b261f0d548b75dd5bd3ae9a7d9d1e2fb252b3da8
David <143638937+Cheessse@users.noreply.github.com>
2024-04-13 11:14:23 +0100

    update

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,15 +1,18 @@
-import css from './ImageCard.module.css'
+import css from "./ImageCard.module.css";
 
-const ImageCard = ({ image }) => {
-    return (
-        <div className={css.container}>
-                <img
-                    src={image.urls.small}
-                    alt={image.alt_description}
-                    className={css.image}
-                />
-        </div>
-    )
-}
+const ImageCard = ({ image, onImageClick }) => {
+  return (
+    <div className={css.container}>
+      <img
+        src={image.urls.small}
+        alt={image.alt_description}
+        className={css.image}
+        onClick={onImageClick}
+        width="320"
+        height="200"
+      />
+    </div>
+  );
+};
 
-export default ImageCard
+export default ImageCard;
